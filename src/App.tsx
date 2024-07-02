8db784ab82f6bb419ed6fa30ef55408a7f81d306
dolphin-pc <pcx474@gmail.com>
2024-07-02 11:50:56 +0900

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button, Input, Modal, Select } from "./components";
+
+function App() {
+  return (
+    <>
+      <Button />
+      <Input />
+      <Modal />
+      <Select />
+    </>
+  );
+}
+
+export default App;
