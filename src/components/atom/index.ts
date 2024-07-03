c9fd7adb3057c98a361259b5623069322196cc51
VG20-s <pkll31311@gmail.com>
2024-07-03 20:18:01 +0900

    select 구현 절반완료

--- src/components/atom/index.ts
+++ src/components/atom/index.ts
@@ -1,4 +1,4 @@
 export { default as Input } from "./Input";
 // export { default as Button } from "./Button";
 export { default as Modal } from "./Modal";
-export { default as Select } from "./Select";
+// export { default as Select } from "./Select";
--- src/styles/index.style.ts
+++ src/styles/index.style.ts
@@ -1,2 +1,3 @@
 export * as button from "./button.style";
 export * as row from "./row.style";
+export * as select from "./select.style";
